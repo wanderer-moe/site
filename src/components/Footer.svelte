f5f6a5b886d4878a02a728dcf8cb7ff9e1308b14
dromzeh <65048232+dromzeh@users.noreply.github.com>
2022-07-17 01:59:44 +0100

    test update footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -6,7 +6,8 @@
 
 <footer class="mt-auto p-4 bg-[#19191E] shadow md:flex md:items-center md:justify-between md:p-6">
     <span class="text-sm sm:text-left text-gray-400">© 2022 - created & programmed by <a href="https://dromzeh.dev/" class="hover:underline">dromzeh</a>.
-        <br>Website is not affiliated with or endorsed by <a href="https://www.hoyoverse.com/en-us/" class="hover:underline">HoYoverse</a>.</span>
-<!-- <div class="flex flex-wrap items-center mt-3 gap-3">
+        <br>Website is not affiliated with or endorsed by <a href="https://www.hoyoverse.com/en-us/" class="hover:underline">HoYoverse</a>.
+        <a href="privacy-policy/" class="hover:underline">Privacy Policy</a></span>
+    <!-- <div class="flex flex-wrap items-center mt-3 gap-3">
     </div> -->
 </footer>
