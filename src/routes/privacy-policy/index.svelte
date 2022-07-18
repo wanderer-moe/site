bc267b44a28e752ddd28cdc1a33ff117073a5956
dromzeh <65048232+dromzeh@users.noreply.github.com>
2022-07-18 02:39:49 +0100

    update

--- src/routes/privacy-policy/index.svelte
+++ src/routes/privacy-policy/index.svelte
@@ -11,7 +11,7 @@
 	<p class = "text-white">• This site does <span class = underline>not</span> collect, store or share any personally identifiable information.</p>
 	<p class = "text-white">• However, this site does use Google Analytics - it collects information about country, device & visited urls. This information isn't shared to any third parties.</p>
 	<p class = "text-white">• If you would like to opt out of Google Analytics, there is an <span class = "text-blue-500 font-semibold"><a href = "https://tools.google.com/dlpage/gaoptout">add-on</a></span> you can install.</p>
-	<p class = "text-white">• Google Adsense adverts are also used to help fund hosting for the website. You can opt out of personalised ads at anytime.</p>
+	<p class = "text-white">• Adverts may also be served, this is to help fund hosting for the website. You can opt out of personalised ads at anytime.</p>
 	<br>
 	<p class = "text-white">If you would like to exercise your rights, please let us know by getting in touch @ <a href = "mailto:admin@wanderer.moe" class = "font-semibold text-blue-500">admin@wanderer.moe</a> or <a href = "mailto:admin@dromzeh.dev" class = "font-semibold text-blue-500">admin@dromzeh.dev</a></p>
 	</div>
