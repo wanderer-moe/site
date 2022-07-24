8fff4eaabda92a981180d6b92a02fa8ffcf2e9d1
dromzeh <65048232+dromzeh@users.noreply.github.com>
2022-07-24 15:20:36 +0100

    add search feature (i did not test this beforehand)

--- src/routes/characterparts/index.svelte
+++ src/routes/characterparts/index.svelte
@@ -8,6 +8,21 @@
 <script>
 export let data;
 let characterParts = data.characterParts;
+
+
+let searchTerm = "";
+let filteredCharacterParts = [];
+
+$: {
+	if(searchTerm){
+		filteredCharacterParts = characterParts.filter(characterPart => {
+			return characterPart.toLowerCase().includes(searchTerm.toLowerCase().replace(" ", "-"));
+		});
+	} else {
+		filteredCharacterParts = characterParts;
+	}
+}
+
 </script>
 	
 <style>
@@ -26,8 +41,14 @@ img {
 	<div class="container px-4 mx-auto">
 	<h1 class = "text-white font-semibold gifont">Character Parts</h1>
 	<p class = "text-white">Character parts extracted from the Genshin Preview Pages, if there's a file you can't find, download the <span class = "text-blue-500"><a href = "/splashart" class = "text-blue-500 hover:underline">splash art version</a></span> instead.</p>
-	<p class = "text-gray-400 text-sm">These are manually updated from a <a href = "https://github.com/dromzeh/genshin-character-parts" class = "hover:underline">github repository</a> & it may take from 0-24 hours for new parts to appear.<br>The preview images may take a while to load dependant on your internet connection.<br>Clicking 'Download File' will take you to a new page where you can save, copy, or download the file.</p>
+	<p class = "text-gray-400 text-sm">The preview images may take a while to load dependant on your internet connection. Clicking 'Download File' will take you to a new page where you can save, copy, or download the file.</p>
 	<br>
+
+	<div class = "flex items-center justify-center">
+		<input class="bg-[#2A303C] text-blue-400 h-14 w-96 rounded text-center focus:shadow focus:outline-none" bind:value ={searchTerm} placeholder="Search">
+	</div>
+	<br>
+
 	<div class = "text-right">
 	<a href = "https://github.com/dromzeh/genshin-character-parts/archive/refs/heads/main.zip">
 		<button class="bg-gray-900 font-semibold text-white p-0.5 mb-2 mr-2 rounded-lg text-center px-5 py-2.5 hover:bg-gray-600 focus:shadow focus:outline-none">Download All Files</button>
@@ -37,7 +58,7 @@ img {
 
 	<div class="flex flex-wrap justify-center gap-5">
 
-		{#each characterParts as entry}
+		{#each filteredCharacterParts as entry}
 		<div class="w-full md:w-1/3 xl:w-auto px-4 mb-8 md:mb-0">
 		  <div class="h-full max-w-xs mx-auto p-12 bg-[#2A303C] transition duration-150 ease-in-out hover:scale-105 rounded-xl">
 			<div class="mb-7">
@@ -51,6 +72,11 @@ img {
 			</div>
 		  </div>
 		  {/each}
+
+		  <!-- check if filteredCharacterParts is empty -->
+		  {#if filteredCharacterParts.length === 0}
+		  <p class = "text-white text-center">No results found for '{searchTerm}'</p>
+		  {/if}
 		</div>
 	</div>
 
--- src/routes/splashart/index.svelte
+++ src/routes/splashart/index.svelte
@@ -1,13 +1,27 @@
 <script context="module">
 	export async function preload(page) {
-		const data = await import(`../../data/splashArt.json`);
+		const data = await import(`../../data/splashArts.json`);
 		return { data }; // id will be returned for displaying images..
 	}
 </script>
 	
 <script>
 export let data;
-let splashArt = data.splashArt;
+let splashArts = data.splashArts;
+
+let searchTerm = "";
+let filteredSplashArts = [];
+
+	$: {
+		if(searchTerm){
+			filteredSplashArts = splashArts.filter(splashArt => {
+				return splashArt.toLowerCase().includes(searchTerm.toLowerCase().replace(" ", "-"));
+			});
+		} else {
+			filteredSplashArts = splashArts;
+		}
+	}
+
 </script>
 
 <style>
@@ -16,12 +30,19 @@ let splashArt = data.splashArt;
 	<title>splash art | wanderer.moe - a genshin database</title>
 </svelte:head>
 
+<div class = "flex flex-col min-h-screen">
 <section class="py-24 md:py-40">
 	<div class="container px-4 mx-auto">
 	<h1 class = "text-white font-semibold gifont">Character Splash Art</h1>
 	<p class = "text-white">Contains character splash art with and without the background.</p>
-	<p class = "text-gray-400 text-sm">These are manually updated from a <a href = "https://github.com/dromzeh/genshin-splash-art" class = "hover:underline">github repository</a> & it may take from 0-24 hours for new parts to appear.<br>The preview images may take a while to load dependant on your internet connection.<br>Clicking 'Download File' will take you to a new page where you can save, copy, or download the file.</p>
+	<p class = "text-gray-400 text-sm">The images may take a while to load dependant on your internet connection. Clicking 'Download File' will take you to a new page where you can save, copy, or download the file.</p>
 	<br>
+
+	<div class = "flex items-center justify-center">
+		<input class="bg-[#2A303C] text-blue-400 h-14 w-96 rounded text-center focus:shadow focus:outline-none" bind:value ={searchTerm} placeholder="Search">
+	</div>
+	<br>
+
 	<div class = "text-right">
 	<a href = "https://github.com/dromzeh/genshin-splash-art/archive/refs/heads/main.zip">
 		<button class="bg-gray-900 font-semibold text-white p-0.5 mb-2 mr-2 rounded-lg text-center px-5 py-2.5 hover:bg-gray-600 focus:shadow focus:outline-none">Download All Files</button>
@@ -31,7 +52,7 @@ let splashArt = data.splashArt;
 
 	<div class="flex flex-wrap justify-center gap-5">
 
-		{#each splashArt as entry}
+		{#each filteredSplashArts as entry}
 		<div class="w-full md:w-1/3 xl:w-auto px-4 mb-8 md:mb-0">
 		  <div class="h-full max-w-xs mx-auto p-12 bg-[#2A303C] transition duration-150 ease-in-out hover:scale-105 rounded-xl">
 			<div class="mb-7">
@@ -47,7 +68,13 @@ let splashArt = data.splashArt;
 		  </div>
 		  {/each}
 		  
+		  <!-- check if filteredCharacterParts is empty -->
+		{#if filteredSplashArts.length === 0}
+			<p class = "text-white text-center">No results found for '{searchTerm}'</p>
+		{/if}
+
 		</div>
 	</div>
 
 </section>
+</div>
