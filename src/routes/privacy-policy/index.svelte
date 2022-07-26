ef23c07cf35c34602e35ec44611beb78e3bc00a1
dromzeh <65048232+dromzeh@users.noreply.github.com>
2022-07-26 03:28:52 +0100

    yep

--- src/routes/privacy-policy/index.svelte
+++ src/routes/privacy-policy/index.svelte
@@ -13,7 +13,7 @@
 	<p class = "text-white">• If you would like to opt out of Google Analytics, there is an <span class = "text-blue-500 font-semibold"><a href = "https://tools.google.com/dlpage/gaoptout">add-on</a></span> you can install.</p>
 	<p class = "text-white">• Adverts may also be served, this is to help fund hosting for the website. You can opt out of personalised ads at anytime.</p>
 	<br>
-	<p class = "text-white">If you would like to exercise your rights, please let us know by getting in touch @ <a href = "mailto:admin@wanderer.moe" class = "font-semibold text-blue-500">admin@wanderer.moe</a> or <a href = "mailto:admin@dromzeh.dev" class = "font-semibold text-blue-500">admin@dromzeh.dev</a></p>
+	<p class = "text-white">If you would like to exercise your rights, please let us know by getting in touch @ <a href = "mailto:dromzeh@wanderer.moe" class = "font-semibold text-blue-500">dromzeh@wanderer.moe</a></p>
 	</div>
 </section>
 </div>
