74e1f12b1e32501aeea7e17204b2ed159304d3f9
dromzeh <65048232+dromzeh@users.noreply.github.com>
2022-08-01 19:41:58 +0100

    final footer modification i thin,k

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,10 +1,10 @@
 
 <footer class="text-left lg:text-left bg-[#19191E] text-gray-400">
     <div class="flex justify-center items-center lg:justify-between p-6">
-        
-    <div class="mr-12">
+
+    <div class="justify-start mr-12">
         <p>© 2022 - wanderer.moe. Created by <a href = "https://dromzeh.dev/" class = "font-semibold text-white hover:text-blue-400">dromzeh.</a></p>
-        <p>Website isn't affiliated / endorsed by <a href = "https://hoyoverse.com/en-us/" class = "font-semibold text-white hover:text-blue-400">HoYoverse</a>.</p>
+        <p>Website isn't affiliated with <a href = "https://hoyoverse.com/en-us/" class = "font-semibold text-white hover:text-blue-400">HoYoverse</a>.</p>
     </div>
     
     <!--
