9e61025b0056bab60767b79c2c9389906b187e25
dromzeh <65048232+dromzeh@users.noreply.github.com>
2022-08-03 14:43:38 +0100

    add nprogress loading bar

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,30 @@
 <script>
-	import Nav from '../components/Nav.svelte';
-	import Footer from '../components/Footer.svelte';
-	export let segment;
-	segment = segment; // prevent warnings (https://github.com/sveltejs/sapper/issues/824#issuecomment-695624681)
+import Nav from '../components/Nav.svelte';
+import Footer from '../components/Footer.svelte';
+export let segment;
+segment = segment; 
+
+import NProgress from 'nprogress';
+import { navigating } from '$app/stores';
+
+// Progress css
+import 'nprogress/nprogress.css';
+
+NProgress.configure({
+	// Full list: https://github.com/rstacruz/nprogress#configuration
+	minimum: 0.16,
+	showSpinner: false
+	// 3476CF
+});
+
+$: {
+	if ($navigating) {
+		NProgress.start();
+	}
+	if (!$navigating) {
+		NProgress.done();
+	}
+}
 </script>
 
 
