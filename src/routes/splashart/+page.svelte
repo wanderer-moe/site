2fcf120325c489a24eefc5cfbc32f23e22646d7a
dromzeh <65048232+dromzeh@users.noreply.github.com>
2022-11-04 23:53:54 +0000

    fix: i hate programming sometimes.. ¯\_(ツ)_/¯

--- src/routes/splashart/+page.svelte
+++ src/routes/splashart/+page.svelte
@@ -15,7 +15,7 @@ import JSZip from "jszip";
 import saveAs from "file-saver";
 import Toastify from "toastify-js";
 import "toastify-js/src/toastify.css";
-import FAQ from "../../components/FAQ.svelte";
+import Faq from "../../components/Faq.svelte";
 export let data;
 
 let selectedItems = [];
@@ -227,7 +227,7 @@ async function downloadSelected() {
           </p>
         </div>
       {/if}
-      <FAQ />
+      <Faq />
     </div>
   </section>
 </div>
