6d34d67a634d03d0af16919113ab96a7d591c12d
Marcel <65048232+dromzeh@users.noreply.github.com>
2023-05-25 18:40:12 +0100

    feat: add sort by date, filesize, name & ul date options
    - add sorting from filesize small to large & large to small
    - name from a-z and z-a
    - upload date from newest uploaded to oldest uploaded
    - call & store selected sorting value in localstorage

--- src/lib/utils/helpers.js
+++ src/lib/utils/helpers.js
@@ -11,3 +11,27 @@ export function bytesToFileSize(bytes) {
   const i = Math.floor(Math.log2(bytes) / 10);
   return `${(bytes / 1024 ** i).toFixed(1)} ${sizes[i]}`;
 }
+
+// ISO 8601 -> DD/Suffix Month YYYY, HH:MM:SS
+export function formatDateReadable(dateString) {
+  const date = new Date(dateString);
+  const day = date.getDate();
+  const month = date.toLocaleString("default", { month: "long" });
+  const year = date.getFullYear();
+  const time = date.toLocaleTimeString([], { hour12: false });
+  const suffix = getOrdinalSuffix(day);
+  return `${day}${suffix} ${month} ${year}, ${time}`;
+}
+function getOrdinalSuffix(day) {
+  const suffixes = ["th", "st", "nd", "rd"];
+  const lastDigit = day % 10;
+  const suffixIndex =
+    lastDigit === 1 ? 1 : lastDigit === 2 ? 2 : lastDigit === 3 ? 3 : 0;
+  return suffixes[suffixIndex];
+}
+
+// ISO 8601 -> Unix Timestamp
+export function iso8601ToUnix(isoString) {
+  const date = new Date(isoString);
+  return Math.floor(date.getTime() / 1000);
+}
