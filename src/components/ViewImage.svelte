25965005c132e195c89ec94c2e46b5d30784b518
Jason <123jasont@gmail.com>
2023-05-29 14:00:57 -0700

    style: add background to `ViewImage`

--- src/components/ViewImage.svelte
+++ src/components/ViewImage.svelte
@@ -8,15 +8,17 @@ export let closeImageView
 </script>
 
 <div
-    class="fixed left-1/2 top-1/2 z-[1000] max-h-[90%] max-w-[90%] -translate-x-1/2 -translate-y-1/2 bg-blend-overlay">
+    class="fixed left-1/2 top-1/2 z-[1500] max-h-[90%] max-w-[90%] -translate-x-1/2 -translate-y-1/2 bg-blend-overlay">
     <div
         in:fly="{{ y: 50, easing: quintOut, duration: 750 }}"
-        out:fly="{{ y: 50, easing: cubicOut, duration: 300 }}">
+        out:fly="{{ y: 50, easing: cubicOut, duration: 300 }}"
+        class="bg-stone-950 rounded-md p-4 text-center">
         <img
             src="{imageUrl}"
-            class="h-64 w-64 object-contain"
-            alt="Full view" />
-        <p class="sigfont mt-4 text-center text-white">
+            class="h-[40vh] object-contain border border-stone-900 rounded-md"
+            alt="Full view"
+            style="background-image: url(https://files.catbox.moe/qmjf27.png); background-size: contain;" />
+        <p class="mt-4 rounded-md bg-[#141414] py-2 text-center text-white">
             {$t('viewImage.closeText')}
         </p>
     </div>
