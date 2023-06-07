c94ec603d0962b2f47df51c01b2e74ee11188924
Marcel <65048232+dromzeh@users.noreply.github.com>
2023-06-07 08:00:31 +0100

    feat: replace google analytics with cloudflare

--- src/routes/privacy-policy/+page.svelte
+++ src/routes/privacy-policy/+page.svelte
@@ -11,23 +11,14 @@
             <div>
                 <div class="m-2 grid gap-3">
                     <p class="text-white">
-                        <b
-                            >No data about what you download or what you
-                            specifically do on the site is collected for any
-                            reason</b
-                        >. Google Analytics is used to track page views, but no
-                        personally identifiable information is stored for any
-                        purpose.
+                        Cloudflare Web Analytics is used to collect analytics
+                        data, but is anonymized and not traceable back to any
+                        specific user.
                     </p>
                     <p class="text-white">
                         Your IP address <b
-                            >may be collected by Cloudflare for security
+                            >may also be collected by Cloudflare for security
                             purposes, but this is not stored by the site.</b>
-                    </p>
-                    <p class="text-white">
-                        Any data collected by Google Analytics is not shared
-                        with anyone, and kept private out of respect for your
-                        privacy.
                     </p>
                     <p class="text-white">
                         If you have any questions, etc, feel free to contact <a
