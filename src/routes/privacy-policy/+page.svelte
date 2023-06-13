b0289cd0b3bbba7925b0a77ea118091a28c862b4
Marcel <65048232+dromzeh@users.noreply.github.com>
2023-06-13 11:05:58 +0100

    feat: update tos & privacy policy

--- src/routes/privacy-policy/+page.svelte
+++ src/routes/privacy-policy/+page.svelte
@@ -8,15 +8,16 @@
             <p class="m-2 text-left text-4xl font-semibold text-white">
                 Privacy Policy
             </p>
+            <p class="m-2 text-white">Last updated: 12th June 2023</p>
             <div>
-                <div class="m-2 grid gap-3">
-                    <p class="text-white">
+                <div class="m-2 mt-4 grid gap-3">
+                    <p class="text-gray-400">
                         Cloudflare Web Analytics is used to collect analytics
-                        data, but is <span class="font-semibold"
+                        data, but is <span class="font-semibold text-white"
                             >anonymized and not traceable back to any specific
                             user.</span>
                     </p>
-                    <p class="text-white">
+                    <p class="text-gray-400">
                         As of 8th June 2023, anonymized data about requests made
                         to the Website's API will be collected.
                         <span class="font-semibold"
@@ -25,14 +26,14 @@
                         This is used to store most popular games and assets pages,
                         to sort by popularity.
                     </p>
-                    <p class="text-white">
-                        Your IP address <b
+                    <p class="text-gray-400">
+                        Your IP address <span class="font-semibold text-white"
                             >may also be collected by Cloudflare for security
-                            purposes, but this is not stored by the site.</b>
+                            purposes, but this is not stored by the site.</span>
                     </p>
-                    <p class="text-white">
+                    <p class="mt-4 text-gray-400">
                         If you have any questions, etc, feel free to contact <a
-                            class="hover:font-semibold hover:text-white"
+                            class="text-white hover:font-semibold"
                             href="mailto:privacy@wanderer.moe"
                             >privacy@wanderer.moe</a> and you'll recieve a response
                         within 24 hours.
--- src/routes/terms-of-service/+page.svelte
+++ src/routes/terms-of-service/+page.svelte
@@ -11,36 +11,36 @@
             <p class="m-2 text-white">Last updated: 12th June 2023</p>
             <div>
                 <div class="m-2 mt-4 grid gap-3">
-                    <p class="text-white">
+                    <p class="text-gray-400">
                         By using wanderer.moe and any respective subdomains, the
                         following terms are agreed to. You are not allowed to
                         use this website if you do not agree to these terms.
                     </p>
-                    <p class="text-white">
-                        We reserve <span class="font-semibold"
+                    <p class="text-gray-400">
+                        We reserve <span class="font-semibold text-white"
                             >the right to modify these Terms at any time</span
                         >. For instance, we may need to change these Terms if we
                         come out with a new feature or for some other reason.
                     </p>
-                    <p class="text-white">
+                    <p class="text-gray-400">
                         The website is provided "as-is", and the owner of the
                         website is not responsible for any damages caused.
                     </p>
-                    <p class="text-white">
+                    <p class="text-gray-400">
                         From time to time this website may also include links to
                         other websites. These links are provided for your
                         convenience to provide further information. They do not
                         signify that we endorse the website(s). We have no
                         responsibility for the content of the linked website(s).
                     </p>
-                    <p class="text-white">
+                    <p class="text-gray-400">
                         Your use of any information or materials on this website
                         is entirely at your own risk, for which we shall not be
                         liable. It shall be your own responsibility to ensure
                         that any products, services or information available
                         through this website meet your specific requirements.
                     </p>
-                    <p class="text-white">
+                    <p class="text-gray-400">
                         All content published and made available on our site,
                         including images, text, logos, documents, downloadable
                         files, and any other components that contribute to the
@@ -51,28 +51,28 @@
                         with these companies and do not claim ownership over
                         their assets.
                     </p>
-                    <p class="text-white">
+                    <p class="text-gray-400">
                         Your use of our site does not grant you ownership of any
                         content you may access on the site.
                     </p>
-                    <p class="text-white">
+                    <p class="text-gray-400">
                         No personally identifiable information is collected by
                         this website, please read the <a
-                            class="font-semibold"
+                            class="text-white hover:font-semibold"
                             href="/privacy-policy">Privacy Policy</a> for more information.
                     </p>
-                    <p class="text-white">
+                    <p class="text-gray-400">
                         The website's codebase is completely open source, and is
                         licensed under GPL-3.0. You must state all significant
                         changes made to the original software, make the source
                         code available to the public with credit to the original
                         author, original source, and use the same license.
                     </p>
-                    <p class="mt-4 text-white">
+                    <p class="mt-4 text-gray-400">
                         If you believe that your intellectual property rights
                         have been infringed upon by our website content, please
                         contact <a
-                            class="font-semibold"
+                            class="text-white hover:font-semibold"
                             href="mailto:marcel@wanderer.moe"
                             >marcel@wanderer.moe</a
                         >, where your claim will be reviewed. If your claim is
