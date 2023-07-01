a69887a5632d1ae8d22e75b63be63aff7d73ba1f
Marcel <65048232+dromzeh@users.noreply.github.com>
2023-07-01 20:46:56 +0100

    feat: oc generator design

--- src/routes/oc-generator/[game]/+page.svelte
+++ src/routes/oc-generator/[game]/+page.svelte
@@ -3,6 +3,7 @@
 import { onMount } from 'svelte'
 import { fixCasing } from '@/lib/helpers/casing/fixCasing'
 import { writable } from 'svelte/store'
+import { mapGame } from '@/lib/helpers/casing/gameMapping'
 // import { t } from "svelte-i18n"; TODO: i18n support maybe?
 
 export let data
@@ -60,107 +61,113 @@ onMount(async () => {
 </script>
 
 <svelte:head>
-    <title>OC Generator ({fixCasing(game)}) | wanderer.moe</title>
+    <title>OC Generator ({mapGame(game)}) | wanderer.moe</title>
 </svelte:head>
 
 <div class="min-h-screen">
     <div class="mb-8">
-        <div class="relative z-0">
-            <img
-                src="https://cdn.wanderer.moe/{game}/cover.png"
-                class="absolute inset-0 h-48 w-full object-cover transition ease-in-out"
-                style="object-position: 50% 20%;"
-                alt="{game} cover" />
-            <div
-                class="relative h-48 bg-gradient-to-t from-main-400 to-main-400/50">
-                <div
-                    class="mx-auto px-4 py-16 sm:max-w-xl md:max-w-full md:px-24 lg:max-w-screen-xl lg:px-8">
+        <div class="px-2 md:px-12 lg:px-48">
+            <div class="relative my-8">
+                <img
+                    src="https://cdn.wanderer.moe/{game}/cover.png"
+                    class="absolute inset-0 h-48 w-full rounded-md object-cover"
+                    style="object-position: 50% 20%;"
+                    alt="{game} cover" />
+                <div class="relative h-48 rounded-md bg-main-400/50">
                     <div
-                        class="flex flex-col items-center justify-between xl:flex-row">
-                        <div class="">
-                            <h1
-                                class="flex max-w-lg items-start text-3xl font-bold tracking-tight text-white sm:text-4xl sm:leading-none">
-                                <span class="mr-3">{fixCasing(game)}</span>
-                            </h1>
-                            <h2
-                                class="max-w-xl text-xl font-semibold text-white">
-                                OC Generator
-                            </h2>
-                            <p
-                                class="max-w-xl text-sm font-semibold text-white">
-                                You can lock and unlock entries by pressing the <i
-                                    class="fas fa-lock"></i>
-                                and <i class="fas fa-unlock"></i> buttons.
-                            </p>
+                        class="mx-auto px-4 py-16 sm:max-w-xl md:max-w-full md:px-24 lg:max-w-screen-xl lg:px-8">
+                        <div
+                            class="flex flex-col items-center justify-between xl:flex-row">
+                            <div>
+                                <h1
+                                    class="max-w-lg text-3xl font-bold text-white sm:text-4xl sm:leading-none">
+                                    {mapGame(game)}
+                                </h1>
+                                <h2 class="max-w-xl text-lg text-white">
+                                    OC Generator
+                                </h2>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="relative z-10 px-2 md:px-12 lg:px-24">
-            <div class="mb-2 rounded-lg bg-main-400 p-2 text-white">
-                <div class="mb-3 flex flex-wrap gap-2 text-sm">
-                    <button class="btn p-2" on:click="{() => randomize()}">
-                        <i class="fa fa-random"></i> Randomize
-                    </button>
-                    <button
-                        class="btn p-2"
-                        on:click="{() => copyToClipboard($options)}">
-                        <i class="fa-regular fa-clipboard"></i> Copy
-                    </button>
-                    <button
-                        class="btn p-2"
-                        on:click="{() =>
-                            options.update((opts) =>
-                                opts.map((opt) => ({ ...opt, locked: true }))
-                            )}">
-                        <i class="fa fa-unlock"></i> Lock All
-                    </button>
-                    <button
-                        class="btn p-2"
-                        on:click="{() =>
-                            options.update((opts) =>
-                                opts.map((opt) => ({ ...opt, locked: false }))
-                            )}">
-                        <i class="fa fa-lock"></i> Unlock All
-                    </button>
-                </div>
+            <div>
                 <div>
-                    <div class="grid grid-cols-1 gap-4 md:grid-cols-2">
-                        {#if $options}
-                            {#each $options as option}
-                                <div class="">
-                                    <p class="font-semibold text-white">
-                                        {option.name}
+                    <div class="mb-2 rounded-lg bg-main-400 p-2 text-white">
+                        <div class="mb-3 flex flex-wrap gap-2 text-sm">
+                            <button
+                                class="btn p-2"
+                                on:click="{() => randomize()}">
+                                <i class="fa fa-random"></i> Randomize
+                            </button>
+                            <button
+                                class="btn p-2"
+                                on:click="{() => copyToClipboard($options)}">
+                                <i class="fa-regular fa-clipboard"></i> Copy
+                            </button>
+                            <button
+                                class="btn p-2"
+                                on:click="{() =>
+                                    options.update((opts) =>
+                                        opts.map((opt) => ({
+                                            ...opt,
+                                            locked: true,
+                                        }))
+                                    )}">
+                                <i class="fa fa-unlock"></i> Lock All
+                            </button>
+                            <button
+                                class="btn p-2"
+                                on:click="{() =>
+                                    options.update((opts) =>
+                                        opts.map((opt) => ({
+                                            ...opt,
+                                            locked: false,
+                                        }))
+                                    )}">
+                                <i class="fa fa-lock"></i> Unlock All
+                            </button>
+                        </div>
+                        <div>
+                            <div class="grid grid-cols-1 gap-4 md:grid-cols-2">
+                                {#if $options}
+                                    {#each $options as option}
+                                        <div class="">
+                                            <p class="font-semibold text-white">
+                                                {option.name}
+                                            </p>
+                                            <div class="flex items-center">
+                                                <button
+                                                    class="btn mr-2 p-2"
+                                                    on:click="{() =>
+                                                        toggleLock(option)}">
+                                                    {#if option.locked}
+                                                        <i class="fas fa-lock"
+                                                        ></i>
+                                                    {:else}
+                                                        <i class="fas fa-unlock"
+                                                        ></i>
+                                                    {/if}
+                                                </button>
+                                                <input
+                                                    type="text"
+                                                    class="h-10 w-full rounded-lg bg-main-600 text-center text-indigo-400 {option.value &&
+                                                    option.locked
+                                                        ? 'text-red-500'
+                                                        : 'text-accent-300'} focus:shadow focus:outline-none"
+                                                    value="{option.value}"
+                                                    readonly />
+                                            </div>
+                                        </div>
+                                    {/each}
+                                {:else}
+                                    <p class="text-white">
+                                        Loading options, try refreshing the page
+                                        if it takes too long.
                                     </p>
-                                    <div class="flex items-center">
-                                        <button
-                                            class="btn mr-2 p-2"
-                                            on:click="{() =>
-                                                toggleLock(option)}">
-                                            {#if option.locked}
-                                                <i class="fas fa-lock"></i>
-                                            {:else}
-                                                <i class="fas fa-unlock"></i>
-                                            {/if}
-                                        </button>
-                                        <input
-                                            type="text"
-                                            class="h-10 w-full rounded-lg bg-main-600 text-center text-indigo-400 {option.value &&
-                                            option.locked
-                                                ? 'text-red-500'
-                                                : 'text-accent-300'} focus:shadow focus:outline-none"
-                                            value="{option.value}"
-                                            readonly />
-                                    </div>
-                                </div>
-                            {/each}
-                        {:else}
-                            <p class="text-white">
-                                Loading options, try refreshing the page if it
-                                takes too long.
-                            </p>
-                        {/if}
+                                {/if}
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
