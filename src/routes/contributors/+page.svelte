1ef08858ae581aa6324828e6353cd60fd4b84311
Marcel <65048232+dromzeh@users.noreply.github.com>
2023-07-02 01:10:58 +0100

    feat: update `/contributors` to match layout

--- src/routes/contributors/+page.svelte
+++ src/routes/contributors/+page.svelte
@@ -1,6 +1,6 @@
 <script>
 import { t } from 'svelte-i18n'
-import { fade } from 'svelte/transition'
+// import { fade } from 'svelte/transition'
 
 export let data
 const { contributors } = data
@@ -50,87 +50,82 @@ function getTopmostCategory(contributor) {
 
 <div class="min-h-screen">
     <div class="mb-8">
-        <div class="relative z-0">
-            <img
-                src="https://cdn.wanderer.moe/goddess-of-victory-nikke/cover.png"
-                class="absolute inset-0 h-48 w-full object-cover transition ease-in-out"
-                style="object-position: 50% 20%;"
-                alt="Cover" />
-            <div
-                class="relative h-48 bg-gradient-to-t from-main-400 to-main-400/50">
-                <div
-                    class="mx-auto px-4 py-16 sm:max-w-xl md:max-w-full md:px-24 lg:max-w-screen-xl lg:px-8">
+        <div class="px-2 md:px-12 lg:px-48">
+            <div class="relative my-8">
+                <img
+                    src="https://cdn.wanderer.moe/goddess-of-victory-nikke/cover.png"
+                    class="absolute inset-0 h-48 w-full rounded-md object-cover"
+                    style="object-position: 50% 20%;"
+                    alt="goddess of victory nikke cover" />
+                <div class="relative h-48 rounded-md bg-main-400/50">
                     <div
-                        class="flex flex-col items-center justify-between xl:flex-row">
-                        <div>
-                            <h1
-                                class="max-w-lg text-3xl font-bold tracking-tight text-white sm:text-4xl sm:leading-none">
-                                {$t('contributors.title')}
-                            </h1>
-                            <h2
-                                class="text-md max-w-xl font-semibold text-white">
-                                {$t('contributors.desc')}
-                            </h2>
+                        class="mx-auto px-4 py-16 sm:max-w-xl md:max-w-full md:px-24 lg:max-w-screen-xl lg:px-8">
+                        <div
+                            class="flex flex-col items-center justify-between xl:flex-row">
+                            <div>
+                                <h1
+                                    class="max-w-lg text-3xl font-bold text-white sm:text-4xl sm:leading-none">
+                                    {$t('contributors.title')}
+                                </h1>
+                                <h2 class="text-lg text-white">
+                                    {$t('contributors.desc')}
+                                </h2>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="relative z-10 mt-8 px-2 md:px-12 lg:px-24">
-            <div class="grid gap-8 md:grid-cols-1 lg:grid-cols-3 lg:px-0">
-                <div class="col-span-3 gap-4">
-                    {#each Object.entries(categories) as [category, { description, roles }]}
-                        <div id="{category}" class="mb-8">
-                            <p class="mb-2 text-3xl font-bold text-white">
-                                {category}
-                            </p>
-                            <p class="text-md mb-2 font-semibold text-white">
-                                {description}
-                            </p>
-                            <div
-                                class="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4">
-                                {#each contributors.filter((contributor) => roles.some( (role) => contributor.roles.includes(role) ) && getTopmostCategory(contributor) === category) as contributor}
-                                    <div
-                                        class="relative flex flex-col items-center justify-center overflow-hidden rounded-md bg-main-500 bg-cover p-2 text-white transition ease-in-out hover:scale-105">
-                                        <a
-                                            href="https://discord.com/users/{contributor.id}"
-                                            target="_blank">
-                                            <img
-                                                src="{contributor.avatar}"
-                                                alt="{contributor.username} avatar"
-                                                class="mb-1 h-16 w-16 rounded-full object-left" />
-                                        </a>
-                                        <p
-                                            class="mb-1 text-center text-sm font-bold text-white">
-                                            {contributor.username}
-                                            {#if contributor.globalname && contributor.globalname !== contributor.username}
-                                                <span
-                                                    class="mb-1 text-center text-xs font-semibold text-gray-400">
-                                                    aka "{contributor.globalname}"
-                                                </span>
-                                            {/if}
-                                        </p>
+            <div>
+                {#each Object.entries(categories) as [category, { description, roles }]}
+                    <div id="{category}" class="mb-8">
+                        <p class="mb-2 text-3xl font-bold text-white">
+                            {category}
+                        </p>
+                        <p class="text-md mb-2 text-white">
+                            {description}
+                        </p>
+                        <div class="grid grid-cols-2 gap-4 md:grid-cols-3">
+                            {#each contributors.filter((contributor) => roles.some( (role) => contributor.roles.includes(role) ) && getTopmostCategory(contributor) === category) as contributor}
+                                <div
+                                    class="relative flex flex-col items-center justify-center overflow-hidden rounded-md bg-main-500 bg-cover p-2 text-white transition ease-in-out hover:scale-105">
+                                    <a
+                                        href="https://discord.com/users/{contributor.id}"
+                                        target="_blank">
+                                        <img
+                                            src="{contributor.avatar}"
+                                            alt="{contributor.username} avatar"
+                                            class="mb-1 h-16 w-16 rounded-full object-left" />
+                                    </a>
+                                    <p
+                                        class="mb-1 text-center text-sm font-bold text-white">
+                                        {contributor.username}
+                                        {#if contributor.globalname && contributor.globalname !== contributor.username}
+                                            <span
+                                                class="mb-1 text-center text-xs font-semibold text-gray-400">
+                                                aka "{contributor.globalname}"
+                                            </span>
+                                        {/if}
+                                    </p>
 
-                                        <div
-                                            class="flex flex-wrap justify-center gap-x-1 gap-y-1">
-                                            {#each contributor.roles as role}
-                                                <div
-                                                    class="flex items-center rounded-md bg-main-400 px-2 py-1 text-xs text-gray-400">
-                                                    <span
-                                                        class="{roleColors[
-                                                            role
-                                                        ]} mr-1 h-2 w-2 rounded-full"
-                                                    ></span>
-                                                    {role}
-                                                </div>
-                                            {/each}
-                                        </div>
+                                    <div
+                                        class="flex flex-wrap justify-center gap-x-1 gap-y-1">
+                                        {#each contributor.roles as role}
+                                            <div
+                                                class="flex items-center rounded-md bg-main-400 px-2 py-1 text-xs text-gray-400">
+                                                <span
+                                                    class="{roleColors[
+                                                        role
+                                                    ]} mr-1 h-2 w-2 rounded-full"
+                                                ></span>
+                                                {role}
+                                            </div>
+                                        {/each}
                                     </div>
-                                {/each}
-                            </div>
+                                </div>
+                            {/each}
                         </div>
-                    {/each}
-                </div>
+                    </div>
+                {/each}
             </div>
         </div>
     </div>
