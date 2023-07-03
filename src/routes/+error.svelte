c882358660c5207577c814e72e1454cab564488c
Marcel <65048232+dromzeh@users.noreply.github.com>
2023-07-03 05:34:32 +0100

    simplfy `+error.svelte`

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -12,25 +12,20 @@ import { page } from '$app/stores'
         <div class="p-1">
             <p class="p-2 text-5xl font-semibold">
                 <span class="rounded-lg bg-red-400 pl-3 pr-3 text-main-400"
-                    >{$page.status}</span>
+                    >{$page.status}
+                </span>
             </p>
         </div>
         <div>
-            {#if $page.status === 404}
-                <p class="text-2xl font-semibold text-white">
-                    {$t('errors.404')}
-                </p>
-            {:else}
-                <p class="text-2xl font-semibold text-white">
-                    {$t('errors.500')}
-                </p>
-            {/if}
+            <p class="text-2xl text-white">
+                {$t(`errors.${$page.status === 404 ? '404' : '500'}`)}
+            </p>
             <p class=" text-gray-400">
                 <a href="/" class="hover:font-semibold hover:text-white"
                     >{$t('errors.refresh')}
                 </a>
             </p>
-            <p class="text-xs text-gray-400">{$page.error?.message}</p>
+            <p class="my-3 text-xs text-gray-400">{$page.error?.message}</p>
         </div>
     </div>
 </div>
