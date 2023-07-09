625e33f563fbe8b39bb5f73b066df207bde9976f
Marcel <65048232+dromzeh@users.noreply.github.com>
2023-07-09 03:47:03 +0100

    create `s3.ts` for R2 file uploads to all buckets

---
+++ src/lib/server/s3.ts
@@ -0,0 +1,20 @@
+import {
+    R2_ACCESS_KEY,
+    R2_ACCOUNT_ID,
+    R2_SECRET_KEY,
+} from '$env/static/private'
+import { S3Client } from '@aws-sdk/client-s3'
+
+// uploading to R2, reusable for uploading avatars (a), unverified files (unverified), and verified files (cdn)
+// import { AVATARS_BUCKET_NAME, CDN_BUCKET_NAME, UNVERIFIED_BUCKET_NAME } from "$lib/static/const";
+// import { PutObjectCommand } from '@aws-sdk/client-s3'; && import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+const S3 = new S3Client({
+    region: 'auto',
+    endpoint: `https://${R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+    credentials: {
+        accessKeyId: R2_ACCESS_KEY,
+        secretAccessKey: R2_SECRET_KEY,
+    },
+})
+
+export { S3 }
