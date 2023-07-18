d8531a6351ed90f20494a081d6a95e49d0617f14
Marcel <65048232+dromzeh@users.noreply.github.com>
2023-07-18 02:51:12 +0100

    feat: Prettier, contentlayer, shadcn/ui setup

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+import { withContentlayer } from 'next-contentlayer'
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental: {
+        serverActions: true,
+    },
+}
+
+export default withContentlayer(nextConfig)
