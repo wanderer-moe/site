781e98a27ab2f501ef886054a7299120201ed334
Marcel <65048232+dromzeh@users.noreply.github.com>
2023-07-25 11:02:48 +0100

    recreate status component

--- src/components/layouts/footer/footer.tsx
+++ src/components/layouts/footer/footer.tsx
@@ -2,5 +2,11 @@ import Link from 'next/link'
 import { Status } from '@/components/status'
 
 export function SiteFooter() {
-    return <footer className="w-full border-t bg-background p-3"></footer>
+    return (
+        <footer className="w-full border-t bg-background p-3">
+            <div className="flex flex-row justify-between">
+                <Status />
+            </div>
+        </footer>
+    )
 }
--- src/components/status.tsx
+++ src/components/status.tsx
@@ -1,5 +1,43 @@
+'use client'
+
 import Link from 'next/link'
+import { Button } from '@/components/ui/button'
+import { useEffect, useState } from 'react'
+import { Circle } from 'lucide-react'
 
 export function Status() {
-    return <></>
+    const [status, setStatus] = useState('Pending API Status')
+
+    useEffect(() => {
+        fetch('https://api.wanderer.moe/')
+            .then((res) => res.json())
+            .then((data) => {
+                if (data.status === 'ok') {
+                    setStatus('online')
+                } else {
+                    setStatus('offline')
+                }
+            })
+            .catch((err) => {
+                console.error(err)
+                setStatus('offline')
+            })
+    }, [])
+
+    return (
+        <Link href="https://status.wanderer.moe">
+            <Button variant="outline">
+                <Circle
+                    className={`${
+                        status === 'online' ? 'text-green-200' : 'text-red-200'
+                    } animate-pulse`}
+                    size={12}
+                    fill={status === 'online' ? '#BBF7D0' : '#FECACA'}
+                />
+                <span className="ml-2">
+                    API: {status === 'online' ? 'Online' : 'Degraded'}
+                </span>
+            </Button>
+        </Link>
+    )
 }
