226d02a991b1d94e83bd9d0bc497eb21ae832612
Marcel <65048232+dromzeh@users.noreply.github.com>
2023-07-25 11:39:06 +0100

    game container animations

--- src/components/game/gameContainer.tsx
+++ src/components/game/gameContainer.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import { mapGame, mapAssetType } from '@/lib/helpers/casing/mapping'
 import { useState } from 'react'
+import { motion } from 'framer-motion'
 
 export default function GameContainer(game: any) {
     const [hovered, setHovered] = useState<boolean>(false)
@@ -11,17 +12,36 @@ export default function GameContainer(game: any) {
                 onMouseEnter={() => setHovered(true)}
                 onMouseLeave={() => setHovered(false)}>
                 <div
-                    className="relative flex h-32 items-center justify-center bg-cover text-white transition-all duration-150 ease-in-out"
+                    className="relative flex h-32 items-center justify-center bg-cover text-white transition-all duration-150"
                     style={{
                         backgroundImage: `url(https://cdn.wanderer.moe/${game.name}/cover.png)`,
                         backgroundPosition: '50% 20%',
-                        backgroundSize: hovered ? '105%' : '100%',
+                        backgroundSize: hovered ? '115%' : '100%',
                     }}>
                     <div className="absolute inset-0 bg-black/70"></div>{' '}
                     <div className="align-center absolute">
-                        <p className="text-center text-2xl font-bold text-white">
+                        <motion.p
+                            className="text-center text-2xl font-bold text-white"
+                            animate={{
+                                opacity: hovered ? 1 : 1,
+                                y: hovered ? 0 : 10,
+                            }}
+                            transition={{ duration: 0.15 }}>
                             {mapGame(game.name)}
-                        </p>
+                        </motion.p>
+                        <motion.p
+                            className="text-center text-white"
+                            animate={{
+                                opacity: hovered ? 1 : 0,
+                                y: hovered ? 0 : 10,
+                            }}
+                            transition={{ duration: 0.15 }}>
+                            {game.assetCategories.length}{' '}
+                            {game.assetCategories.length === 1
+                                ? 'category'
+                                : 'categories'}{' '}
+                            available
+                        </motion.p>
                     </div>
                 </div>
             </div>
