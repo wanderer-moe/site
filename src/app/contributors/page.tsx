6fb041e49f83274f96af000ba5ecaf9cd46ca8b6
Jason <123jasont@gmail.com>
2023-07-31 18:59:00 -0700

    lint: prettier

--- src/app/contributors/page.tsx
+++ src/app/contributors/page.tsx
@@ -62,63 +62,87 @@ export default function Page() {
     return (
         <main className="min-h-screen p-5 md:px-16 lg:px-48">
             <div className="flex flex-col items-center justify-center">
-                <div className="relative mt-8 px-2 grid gap-8 md:grid-cols-1 lg:grid-cols-3 lg:px-0">
+                <div className="relative mt-8 grid gap-8 px-2 md:grid-cols-1 lg:grid-cols-3 lg:px-0">
                     <div className="col-span-3 gap-4">
                         {Object.entries(categories).map(
                             ([category, { description, roles, icon }]) => (
-                                <div key={category} id={category} className="mb-8">
-                                    <span className="flex items-center mb-2 gap-2">
-                                        <span className="inline-block">{icon}</span>
-                                        <p className="text-3xl font-bold text-white">{category}</p>
+                                <div
+                                    key={category}
+                                    id={category}
+                                    className="mb-8">
+                                    <span className="mb-2 flex items-center gap-2">
+                                        <span className="inline-block">
+                                            {icon}
+                                        </span>
+                                        <p className="text-3xl font-bold text-white">
+                                            {category}
+                                        </p>
                                     </span>
-                                    <p className="text-md mb-4 font-semibold text-white">{description}</p>
+                                    <p className="text-md mb-4 font-semibold text-white">
+                                        {description}
+                                    </p>
                                     <div className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4">
                                         {contributors
                                             .filter(
                                                 (contributor) =>
-                                                    roles.some((role) => contributor.roles.includes(role)) &&
-                                                    getTopmostCategory(contributor) === category
+                                                    roles.some((role) =>
+                                                        contributor.roles.includes(
+                                                            role,
+                                                        ),
+                                                    ) &&
+                                                    getTopmostCategory(
+                                                        contributor,
+                                                    ) === category,
                                             )
                                             .map((contributor) => (
                                                 <div
                                                     key={contributor.id}
-                                                    className="relative flex flex-col items-center justify-center overflow-hidden rounded-md border bg-cover p-2 text-white transition ease-in-out hover:scale-105"
-                                                >
-                                                    <a href={`https://discord.com/users/${contributor.id}`} target="_blank" rel="noreferrer">
+                                                    className="relative flex flex-col items-center justify-center overflow-hidden rounded-md border bg-cover p-2 text-white transition ease-in-out hover:scale-105">
+                                                    <a
+                                                        href={`https://discord.com/users/${contributor.id}`}
+                                                        target="_blank"
+                                                        rel="noreferrer">
                                                         <img
-                                                            src={contributor.avatar}
+                                                            src={
+                                                                contributor.avatar
+                                                            }
                                                             alt={`${contributor.username} avatar`}
                                                             className="mb-1 h-16 w-16 rounded-full object-left"
                                                         />
                                                     </a>
-                                                    <p className="flex items-center justify-center text-sm font-bold text-white mb-1 gap-1">
+                                                    <p className="mb-1 flex items-center justify-center gap-1 text-sm font-bold text-white">
                                                         {contributor.username}
                                                         {contributor.globalname &&
-                                                            contributor.globalname !== contributor.username && (
+                                                            contributor.globalname !==
+                                                                contributor.username && (
                                                                 <span className="text-xs font-semibold text-gray-400">
-                                                                    aka "{contributor.globalname}"
+                                                                    aka "
+                                                                    {
+                                                                        contributor.globalname
+                                                                    }
+                                                                    "
                                                                 </span>
                                                             )}
                                                     </p>
 
                                                     <div className="flex flex-wrap justify-center gap-x-1 gap-y-1">
-                                                        {contributor.roles.map((role) => (
-                                                            <div
-                                                                key={role}
-                                                                className="flex items-center rounded-md border px-2 py-1 text-xs text-gray-400"
-                                                            >
-                                                                <span
-                                                                    className={`${roleColors[role]} mr-1 h-2 w-2 rounded-full`}
-                                                                ></span>
-                                                                {role}
-                                                            </div>
-                                                        ))}
+                                                        {contributor.roles.map(
+                                                            (role) => (
+                                                                <div
+                                                                    key={role}
+                                                                    className="flex items-center rounded-md border px-2 py-1 text-xs text-gray-400">
+                                                                    <span
+                                                                        className={`${roleColors[role]} mr-1 h-2 w-2 rounded-full`}></span>
+                                                                    {role}
+                                                                </div>
+                                                            ),
+                                                        )}
                                                     </div>
                                                 </div>
                                             ))}
                                     </div>
                                 </div>
-                            )
+                            ),
                         )}
                     </div>
                 </div>
--- src/components/layouts/footer/footer.tsx
+++ src/components/layouts/footer/footer.tsx
@@ -7,11 +7,10 @@ export function SiteFooter() {
     const currentYear = new Date().getFullYear()
 
     return (
-        <footer
-            className="border-t p-3 text-gray-400 backdrop-blur-lg backdrop-filter transition-opacity">
+        <footer className="border-t p-3 text-gray-400 backdrop-blur-lg backdrop-filter transition-opacity">
             <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
                 <div className="md:flex md:justify-between">
-                    <div className="flex flex-col mb-4 md:mb-0">
+                    <div className="mb-4 flex flex-col md:mb-0">
                         <div className="mt-2 grid gap-2">
                             <Status />
                             <Discord />
@@ -19,57 +18,68 @@ export function SiteFooter() {
                     </div>
                     <div className="grid grid-cols-2 gap-8 sm:grid-cols-3 sm:gap-6">
                         <div>
-                            <p className="mb-4 font-semibold text-white">Socials</p>
+                            <p className="mb-4 font-semibold text-white">
+                                Socials
+                            </p>
                             <ul>
                                 <li className="mb-2">
                                     <a
                                         href="https://git.wanderer.moe/"
-                                        className="hover:text-white"
-                                    >GitHub</a>
+                                        className="hover:text-white">
+                                        GitHub
+                                    </a>
                                 </li>
                                 <li>
                                     <a
                                         href="https://discord.wanderer.moe/"
-                                        className="hover:text-white"
-                                    >Discord Server</a>
+                                        className="hover:text-white">
+                                        Discord Server
+                                    </a>
                                 </li>
                             </ul>
                         </div>
                         <div>
-                            <p className="mb-4 font-semibold text-white">Legal</p>
+                            <p className="mb-4 font-semibold text-white">
+                                Legal
+                            </p>
                             <ul>
                                 <li className="mb-2">
                                     <a
                                         href="/legal/privacy-policy"
-                                        className="hover:text-white"
-                                    >Privacy Policy</a>
+                                        className="hover:text-white">
+                                        Privacy Policy
+                                    </a>
                                 </li>
                                 <li className="mb-2">
                                     <a
                                         href="/legal/terms-of-service"
-                                        className="hover:text-white"
-                                    >Terms of Service</a>
+                                        className="hover:text-white">
+                                        Terms of Service
+                                    </a>
                                 </li>
                             </ul>
                         </div>
                         <div>
-                            <p className="mb-4 font-semibold text-white">Other</p>
+                            <p className="mb-4 font-semibold text-white">
+                                Other
+                            </p>
                             <ul>
                                 <li className="mb-2">
                                     <a
                                         href="https://wanderer.moe/asset-request-form"
-                                        className="hover:text-white"
-                                    >Asset Request Form</a>
+                                        className="hover:text-white">
+                                        Asset Request Form
+                                    </a>
                                 </li>
                                 <li className="mb-2">
                                     <a
                                         href="/contributors"
-                                        className="hover:text-white"
-                                    >Contributors</a>
+                                        className="hover:text-white">
+                                        Contributors
+                                    </a>
                                 </li>
                                 <li className="mb-2">
-                                    <p
-                                        className="cursor-pointer hover:text-white">
+                                    <p className="cursor-pointer hover:text-white">
                                         Contribute
                                     </p>
                                 </li>
@@ -77,14 +87,15 @@ export function SiteFooter() {
                         </div>
                     </div>
                 </div>
-                <hr className="my-6 border-main-300 sm:mx-auto lg:my-8" />
-                <div className="flex flex-col text-center text-xs gap-1">
-                    <span className="text-white">wanderer.moe © 2022-{currentYear}</span>
+                <hr className="border-main-300 my-6 sm:mx-auto lg:my-8" />
+                <div className="flex flex-col gap-1 text-center text-xs">
+                    <span className="text-white">
+                        wanderer.moe © 2022-{currentYear}
+                    </span>
                     <span>
                         <p>
-                            Not affiliated with any of
-                            the games or companies listed on this site unless
-                            otherwise stated.
+                            Not affiliated with any of the games or companies
+                            listed on this site unless otherwise stated.
                         </p>
                         <p>
                             Built with{' '}
@@ -92,10 +103,28 @@ export function SiteFooter() {
                                 className="inline-block h-3 w-3 text-gray-400"
                                 fill="currentColor"
                             />{' '}
-                            using <Link className="text-white" href="https://nextjs.org/">Next.js</Link> &{' '}
-                            <Link className="text-white" href="https://cloudflare.com/">Cloudflare</Link>{' '}
-                            by <Link className="text-white" href="https://dromzeh.dev/">dromzeh</Link> and
-                            our <Link className="text-white" href="/contributors">contributors.</Link>
+                            using{' '}
+                            <Link
+                                className="text-white"
+                                href="https://nextjs.org/">
+                                Next.js
+                            </Link>{' '}
+                            &{' '}
+                            <Link
+                                className="text-white"
+                                href="https://cloudflare.com/">
+                                Cloudflare
+                            </Link>{' '}
+                            by{' '}
+                            <Link
+                                className="text-white"
+                                href="https://dromzeh.dev/">
+                                dromzeh
+                            </Link>{' '}
+                            and our{' '}
+                            <Link className="text-white" href="/contributors">
+                                contributors.
+                            </Link>
                         </p>
                     </span>
                 </div>
