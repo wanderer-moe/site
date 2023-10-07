bc2ac821977c720b97fbc1d42c3236af8710dbfe
Marcel <65048232+dromzeh@users.noreply.github.com>
2023-10-08 00:58:16 +0100

    reimpl pw reset page

--- src/app/(account)/account/forgot-password/page.tsx
+++ src/app/(account)/account/forgot-password/page.tsx
@@ -1,10 +1,10 @@
 'use client'
-import { Login } from '@/components/account/login'
+import { ForgotPassword } from '@/components/account/forgot-password'
+import Background from '@/components/background/background'
 import { useAuthContext } from '@/context/auth-context'
 import { redirect } from 'next/navigation'
-import Background from '@/components/background/background'
 
-export default function LoginPage() {
+export default function ForgotPasswordPage() {
     const { isLoadingSession, session } = useAuthContext()
 
     if (session) {
@@ -15,7 +15,11 @@ export default function LoginPage() {
         <div className="mx-auto min-h-screen max-w-screen-xl p-5">
             <Background>
                 <div className="flex min-h-screen flex-row items-center justify-center">
-                    {isLoadingSession ? <div>Loading...</div> : <Login />}
+                    {isLoadingSession ? (
+                        <div>Loading...</div>
+                    ) : (
+                        <ForgotPassword />
+                    )}
                 </div>
             </Background>
         </div>
